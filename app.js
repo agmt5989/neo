a422e91c37d855a93dc4921501fecbb3d41a6f3d
Michael Ajala <agmt5989@gmail.com>
2019-01-26 20:01:34 +0100

    routing

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+
 // Setup
 app.listen(3000);
 console.log('Server started on port 3000');
