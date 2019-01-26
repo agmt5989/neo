bb4f76b718bd8876fc7a1c8373a7c50564260b2a
Michael Ajala <agmt5989@gmail.com>
2019-01-26 20:07:58 +0100

    authenticating neo

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const driver = neo4j.driver('bolt://localhost', )
+const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic)
 
 // Routes
 app.get('/', (req, res) => {
