4339f42d37c90b9aa454ca080bb9fccd9b47af30
Michael Ajala <agmt5989@gmail.com>
2019-01-26 19:47:31 +0100

    express static middleware

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static())
+app.use(express.static(path.join(__dirname, 'public')));
