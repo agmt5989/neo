7ecdea621be28585fc7d92256e19629a37519256
Michael Ajala <agmt5989@gmail.com>
2019-01-26 20:09:23 +0100

    neo4j driver v1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const neo4j = require('neo4j-driver');
+const neo4j = require('neo4j-driver').v1;
 
 const app = express();
 
