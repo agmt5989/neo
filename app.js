6cf9979af404017b1cefdb61ff2b754f84408b0e
Michael Ajala <agmt5989@gmail.com>
2019-01-31 00:36:06 +0100

    boosting things up to 10 mill

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
         '    </tr>\n' +
         '    </thead>\n' +
         '    <tbody>\n');
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 10000000; i++) {
         res.write('<tr>\n' +
             '            <td>' + (i + 1) + '</td>\n' +
             '            <td>' + 2 * (i + 1) + '</td>\n' +
