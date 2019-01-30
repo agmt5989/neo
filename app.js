357d3868e1312d2e4f5225fe95139ebab6bf4f30
Michael Ajala <agmt5989@gmail.com>
2019-01-30 23:36:43 +0100

    creating the frec parameter, with select properties as our keys

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.get('/', (req, res) => {
             onNext: (rec) => {
                 let frec = {
                     id: rec._fields[0].identity.low,
-                    title: record._fields[0].properties.title,
-                    year: record._fields[0].properties.released.low,
+                    title: rec._fields[0].properties.title,
+                    year: rec._fields[0].properties.released.low,
                 };
                 movieArray.push(rec);
                 console.log(rec);
